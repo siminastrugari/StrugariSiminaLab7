cda33528515131d1b60114bcdb5aeedf86f405cd
user <user@DESKTOP-DF0K5CS>
2024-11-25 18:17:27 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace StrugariSiminaLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace StrugariSiminaLab7
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
