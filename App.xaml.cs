479dccd8173d0f9042d94a7570eae299ef4bf896
Simina <siminamarina09@gmail.com>
2024-11-26 13:29:05 +0200

    .

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,15 +1,31 @@
-﻿namespace StrugariSiminaLab7
+﻿using System;
+using StrugariSiminaLab7.Data;
+using System.IO;
+
+namespace StrugariSiminaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
+            MainPage = new AppShell();
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+     
     }
 }
